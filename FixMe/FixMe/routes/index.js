f06303b6dcbfec7abfbcb7c01588eb906dcda5c1
Cody Donelson <cdonelson@orscheln.com>
2018-07-10 07:56:17 -0500

    Initial commit

---
+++ FixMe/FixMe/routes/index.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+var express = require('express');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express', displayMessage: getDisplayMessage() });
+});
+
+module.exports = router;
